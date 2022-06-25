3c3c59e7be51b0be81177afea2c9dc917744d754
caue.palacio <caue.palacio@grupofleury.com.br>
2022-06-24 22:37:26 -0300

    feat: get user coords written in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,50 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Cards } from "./components/Cards";
+import { Search } from "./components/Search";
 
-import { Location } from "./types/types";
+import { useData } from "./providers/DataProvider";
+import axios from "axios";
 
 function App() {
-  const [location, setLocation] = useState<Location>({ lat: 0, long: 0 });
+  const { country, setCountry, setInfo } = useData();
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
-      (position: GeolocationPosition) => {
-        setLocation({
-          lat: position.coords.latitude,
-          long: position.coords.longitude,
-        });
-      },
-      function (error) {
-        if (error.code == error.PERMISSION_DENIED) {
-          console.error("Nenhuma informação de localização disponível");
-        }
+      async (position: GeolocationPosition) => {
+        const data = await axios.get(
+          "https://nominatim.openstreetmap.org/reverse?format=jsonv2&accept-language=en",
+          {
+            params: {
+              lat: position.coords.latitude,
+              lon: position.coords.longitude,
+            },
+          }
+        );
+        setCountry(data.data.address.country);
       }
     );
   }, []);
 
-  return <Cards {...location} />;
+  // async function getDataByCountry(country: string) {
+  //   const address = country.split(" ").join("-");
+
+  //   const info = await axios.get(
+  //     `https://api.covid19api.com/total/country/${address}`
+  //   );
+
+  //   setInfo(info.data);
+  // }
+
+  // useEffect(() => {
+  //   getDataByCountry(country);
+  // }, [country]);
+
+  return (
+    <>
+      <Search />
+      <Cards />
+    </>
+  );
 }
 
 export default App;
