f0cd52fb7e62920964ca237189eb203bcbcece40
caue.palacio <caue.palacio@grupofleury.com.br>
2022-06-25 19:34:31 -0300

    feat: create input autocomplete

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,29 @@
 import axios from "axios";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { useData } from "../providers/DataProvider";
+import { allCountries } from "../utils/utils";
+
+interface Countries {
+  name: string;
+  code: string;
+}
 
 export function Search() {
   const { setCountry } = useData();
 
   const [value, setValue] = useState<string>("");
+  const [search, setSearch] = useState<Countries[]>([]);
+
+  function findCountryInSearch(e: FormEvent<HTMLInputElement>) {
+    const newValue = e.currentTarget.value.toUpperCase();
+
+    const search = allCountries.filter((country) =>
+      country.name.toUpperCase().includes(newValue)
+    );
+
+    setSearch(search);
+    setValue(e.currentTarget.value);
+  }
 
   async function searchDataWithText(e: { preventDefault: () => void }) {
     e.preventDefault();
@@ -24,10 +42,20 @@ export function Search() {
           type="text"
           name="search"
           id="search-input"
-          onChange={(e) => setValue(e.target.value)}
+          onChange={findCountryInSearch}
+          value={value}
         />
         <button type="submit">CLIQUE AQUI</button>
       </form>
+      {value == ""
+        ? ""
+        : search.map((country) => {
+            return (
+              <div onClick={() => setValue(country.name)} key={country.code}>
+                {country.name}
+              </div>
+            );
+          })}
     </>
   );
 }
