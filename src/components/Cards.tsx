f427e328b905bf81bd67e9a7e8dea81948b5fef0
caue.palacio <caue.palacio@grupofleury.com.br>
2022-06-24 22:40:09 -0300

    fix: cards just handle with its content

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,14 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-
-type Props = {
-  lat: number;
-  long: number;
-};
-
-type APIReturn = {
-  array: Data[];
-};
+import { useData } from "../providers/DataProvider";
 
 type Data = {
   Active: string;
@@ -17,24 +9,10 @@ type Data = {
   Recovered: string;
 };
 
-export const Cards = (location: Props) => {
-  const [data, setData] = useState<Data[]>();
+export const Cards = () => {
   const [today, setToday] = useState<Data>();
-  const [country, setCountry] = useState<string>("");
 
-  async function getUserCountryByCoords() {
-    const data = await axios.get(
-      "https://nominatim.openstreetmap.org/reverse?format=jsonv2&accept-language=en",
-      {
-        params: {
-          lat: location.lat,
-          lon: location.long,
-        },
-      }
-    );
-
-    setCountry(data.data.address.country);
-  }
+  const { country } = useData();
 
   async function getDataFromCountry(country: string) {
     const address = country.split(" ").join("-").toLowerCase();
@@ -42,14 +20,8 @@ export const Cards = (location: Props) => {
     const info = await axios.get(
       `https://api.covid19api.com/total/country/${address}`
     );
-
-    setData(info.data);
     setToday(info.data[info.data.length - 1]);
   }
-
-  useEffect(() => {
-    getUserCountryByCoords();
-  }, [location]);
 
   useEffect(() => {
     if (country != "") getDataFromCountry(country);
@@ -58,29 +30,18 @@ export const Cards = (location: Props) => {
   return (
     <>
       <ul>
-        <li>Confirmados: {today?.Confirmed}</li>
-        <li>Recuperados: {today?.Recovered}</li>
-        <li>Mortes: {today?.Deaths}</li>
-        <li>Ativos: {today?.Active}</li>
-        <li>Dados do dia {new Date().toLocaleDateString()}</li>
+        {today != undefined ? (
+          <>
+            <li>Confirmados: {today.Confirmed}</li>
+            <li>Recuperados: {today.Recovered}</li>
+            <li>Mortes: {today.Deaths}</li>
+            <li>Ativos: {today.Active}</li>
+            <li>Dados do dia {new Date().toLocaleDateString()}</li>
+          </>
+        ) : (
+          <>Nenhuma informação disponível</>
+        )}
       </ul>
-      {data != undefined ? (
-        data.map((item) => {
-          return (
-            <div
-              key={Math.random()}
-              style={{ display: "flex", justifyContent: "space-between" }}
-            >
-              <p key={Math.random()}>{item.Active}</p>
-              <p key={Math.random()}>{item.Confirmed}</p>
-              <p key={Math.random()}>{item.Deaths}</p>
-              <p key={Math.random()}>{item.Recovered}</p>
-            </div>
-          );
-        })
-      ) : (
-        <>Nada para mostrar</>
-      )}
     </>
   );
 };
